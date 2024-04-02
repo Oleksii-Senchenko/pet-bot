9a2dbe1d11a3ce4ea2b5150d7c482549f101e664
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-04-02 23:27:28 +0300

    added debug function

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const TelegramBot = require("node-telegram-bot-api");
+const debug = require("./helpers");
 require("dotenv").config();
-const TOKEN = process.env.TOKEN
+const TOKEN = process.env.TOKEN;
 
 const bot = new TelegramBot(TOKEN, {
   polling: {
     interval: 300,
     autoStart: true,
-    params:{
-      timeout: 10
-    }
+    params: {
+      timeout: 10,
+    },
   },
 });
 
+bot.on("message", (msg) => {
+  const { id } = msg.chat;
 
-bot.on('message', (msg)=>{
-
-console.log(msg);
-
-bot.sendMessage(msg.chat.id, `Здравствуй, ${msg.from.first_name}`)
-})
+  bot
+    .sendMessage(id, debug(msg))
+    .then(() => {
+      console.log("Message has been send");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
